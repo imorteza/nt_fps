82130ef4288556b67fb8576bbb9034b6d38d46df
Thuan Truong <21116370+DoubleT74@users.noreply.github.com>
2019-08-17 13:39:21 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,6 +2,8 @@
 Thanks To siggyfawn
 ]]--
 
+local useColors = true -- Do you want the FPS to change color based on the FPS.
+
 local prevframes, prevtime, curtime, curframes, fps = 0, 0, 0, 0, 0
 
 Citizen.CreateThread(function()
@@ -12,19 +14,29 @@ Citizen.CreateThread(function()
     end
 
     while true do
-        Citizen.Wait(0)
+        Citizen.Wait(10)
         curtime = GetGameTimer()
         curframes = GetFrameCount()
 
-        if((curtime - prevtime) > 1000) then
+        if (curtime - prevtime) > 1000 then
             fps = (curframes - prevframes) - 1              
             prevtime = curtime
             prevframes = curframes
         end
 
-        if fps > 0 and fps < 1000 then
-            displayText("FPS: " .. fps, 0, 255, 255, 255, 255, 0.5, 0.0)
-        end
+        if useColors then
+		    if fps > 0 and fps < 12 then
+		        displayText("~c~FPS: ~r~" .. fps, 0, 255, 255, 255, 255, 0.5, 0.0)
+		    elseif fps > 0 and fps < 30 then
+		        displayText("~c~FPS: ~o~" .. fps, 0, 255, 255, 255, 255, 0.5, 0.0)
+		    elseif fps > 0 and fps < 1000 then
+		        displayText("~c~FPS: ~g~" .. fps, 0, 255, 255, 255, 255, 0.5, 0.0)
+		    end
+		else
+		    if fps > 0 and fps < 1000 then
+		        displayText("~c~FPS: " .. fps, 0, 255, 255, 255, 255, 0.5, 0.0)
+		    end
+		end
     end
 end)
 
